963e9f599dfe1b8d47bfc362419a534c45edda92
Yuki Katsusaka <yuukatsu@ca.info.hiroshima-cu.ac.jp>
2020-10-07 10:43:39 +0900

    コメントを追加

--- showStudentID.py
+++ showStudentID.py
@@ -1,16 +1,18 @@
+# 学籍番号を表示するプログラム
+
 import nfc
 from binascii import hexlify
 
 
 # 学籍番号のサービスコード
-card_service_code = 0x1A8B
+card_service_code = 0x1A8B 
 
 def on_connect(tag):
     idm, pmm = tag.polling(system_code=0xfe00)
     tag.idm, tag.pmm, tag.sys = idm, pmm, 0xfe00
 
     sc = nfc.tag.tt3.ServiceCode(card_service_code >> 6, card_service_code & 0x3f)
-    bc = nfc.tag.tt3.BlockCode(0, service=0)
+    bc = nfc.tag.tt3.BlockCode(0) # ブロック番号は0
     data = tag.read_without_encryption([sc], [bc])
     print ('あなたの学生番号は' + data[4:11].decode() + 'です．')
 
